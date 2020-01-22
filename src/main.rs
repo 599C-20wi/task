045e9342c51ca22fed65424cd10044b4942778d1
Justin Johnson <justinthomasjohnson@gmail.com>
2020-01-22 12:56:44 -0800

    Add request and response messages. (#5)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 #[macro_use]
 extern crate log;
 
-use std::thread;
-use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::thread;
 
-const BUFFER_SIZE: usize = 250;
+use crate::message::{Request, Response};
+
+pub mod message;
+pub mod translate;
+
+const BUFFER_SIZE: usize = 256;
 const PORT: u16 = 3333;
 
 fn handle_client(mut stream: TcpStream) {
     let mut data = [0 as u8; BUFFER_SIZE];
-    while match stream.read(&mut data) {
+    'read: while match stream.read(&mut data) {
         Ok(size) => {
             trace!("stream read {} bytes", size);
-            stream.write(&data[0..size]).unwrap();
+            let request = match Request::deserialize(&data) {
+                Ok(message) => message,
+                Err(e) => {
+                    error!("deserialization failed: {}", e);
+                    continue 'read;
+                }
+            };
+            let response = Response{
+                id: request.id,
+                text: request.text,
+            };
+            stream.write(&response.serialize()).unwrap();
             true
         },
         Err(e) => {
@@ -23,7 +39,6 @@ fn handle_client(mut stream: TcpStream) {
         },
     } {}
 }
-
 
 fn main() {
     simple_logger::init().unwrap();
